0eb52f616179a8522cfc90297262c70fb5a130e4
BT_2S14_02 <henrykdiana@gmail.com>
2024-10-01 10:58:10 -0400

    Created design document, started code

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter your first coordinate point: ");
+
+        String coordinate1 = s.nextLine();
+    }
+}
