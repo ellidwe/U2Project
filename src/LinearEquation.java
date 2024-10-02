5830c8b300a0bebf10c6b93e17d66de3c74c6684
BT_2S14_02 <henrykdiana@gmail.com>
2024-10-02 10:50:56 -0400

    Created design document, started code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -13,9 +13,30 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
+    public double slope()
+    {
+        return ((double) (y2 - y1)) / ((double) (x2 - x1));
+    }
+
+    public double yIntercept()
+    {
+        return y1 - slope() * x1;
+    }
+
+    public double distance()
+    {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
+
     public String toString()
     {
-        return "placeholder";
-    }
+        return "The first coordinate is: (" + x1 + "," + y1 + ")\n"
+                + "The second coordinate is: (" + x2 + "," + y2 + ")\n"
+                + "The equation of the line is: y = " + slope() + "x + " + yIntercept() + "\n"
+                + "The slope of the line is: " + slope() +"\n"
+                + "The y intercept of the line is: " + yIntercept() + "\n"
+                + "The distance between the two points is: " + distance();
+
+     }
 
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -6,7 +6,31 @@ public class LinearEquationRunner {
         Scanner s = new Scanner(System.in);
 
         System.out.print("Enter your first coordinate point: ");
+        String coordinate1 = s.nextLine(); //User enters their first coordinate
 
-        String coordinate1 = s.nextLine();
+        int coordinate1Length = coordinate1.length();
+        String coordinate1Shortened = coordinate1.substring(1, coordinate1Length - 1); //Removes parentheses from first coordinate
+
+        String x1String = coordinate1Shortened.substring(0, coordinate1Shortened.indexOf(",")); //Gets x1 from coordinate
+        int x1 = Integer.parseInt(x1String); //Makes an int with the value of x1
+
+        String y1String = coordinate1Shortened.substring(coordinate1Shortened.indexOf(",") + 1); //Gets y1 from coordinate
+        int y1 = Integer.parseInt(y1String); //Makes an int with the value of y1
+
+        System.out.print("Enter your second coordinate point: ");
+        String coordinate2 = s.nextLine(); //User enters their second coordinate
+
+        int coordinate2Length = coordinate2.length();
+        String coordinate2Shortened = coordinate2.substring(1, coordinate2Length - 1); //Removes parentheses from second coordinate
+
+        String x2String = coordinate2Shortened.substring(0, coordinate2Shortened.indexOf(",")); //Gets x2 from coordinate
+        int x2 = Integer.parseInt(x2String); //Makes an int with the value of x2
+
+        String y2String = coordinate2Shortened.substring(coordinate2Shortened.indexOf(",") + 1); //Gets y2 from coordinate
+        int y2 = Integer.parseInt(y2String); //Makes an int with the value of y2
+
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+
+        System.out.println(equation.toString());
     }
 }
