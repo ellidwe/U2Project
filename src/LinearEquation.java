ff995e69e982b81291db2b79c7109aaec142397e
BT_2S14_02 <eli.jl.dew@gmail.com>
2024-10-07 10:41:26 -0400

    Added basic fraction slope to do: like all the challenge features :(

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -21,7 +21,7 @@ public class LinearEquation {
 
     public String equationSlope()
     {
-        return "placeholder";
+        return (y2 - y1) + "/" + (x2 - x1);
     }
 
     public double yIntercept()
@@ -44,7 +44,7 @@ public class LinearEquation {
     {
         return "The first coordinate is: (" + x1 + "," + y1 + ")\n"
                 + "The second coordinate is: (" + x2 + "," + y2 + ")\n"
-                + "The equation of the line is: y = " + slope() + "x + " + yIntercept() + "\n"
+                + "The equation of the line is: y = " + equationSlope() + "x + " + yIntercept() + "\n"
                 + "The slope of the line is: " + slope() +"\n"
                 + "The y intercept of the line is: " + yIntercept() + "\n"
                 + "The distance between the two points is: " + distance();
